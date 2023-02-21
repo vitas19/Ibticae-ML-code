46fbf75685a332ce748b23292639bc4bee10d779
vbohach <60136209+vitas19@users.noreply.github.com>
2023-02-21 18:17:00 +0100

    Update model.py

--- Object_detection/mrcnn/model.py
+++ Object_detection/mrcnn/model.py
@@ -2,9 +2,6 @@
 Mask R-CNN
 The main Mask R-CNN model implementation.
 
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
 """
 
 import os
@@ -2061,14 +2058,14 @@ class MaskRCNN():
                                  mrcnn_mask, rpn_rois, rpn_class, rpn_bbox],
                              name='mask_rcnn')
 
-        """
+        
         # Add multi-GPU support.
         if config.GPU_COUNT > 1:
             from mrcnn.parallel_model import ParallelModel
             model = ParallelModel(model, config.GPU_COUNT)
 
         return model
-        """
+        
 
     def find_last(self):
         """Finds the last checkpoint file of the last trained model in the
